69afb762f238cb680345c7b3fc1e40b31498d35b
小林優一 <y.k94cofitec179@gmail.com>
2020-04-21 13:27:31 +0900

    環境構築

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+body {
+  font: 14px "Century Gothic", Futura, sans - serif;
+  margin: 20px;
 }
 
-export default App;
+ol, ul {
+  padding - left: 30px;
+}
+
+.board - row: after {
+  clear: both;
+  content: "";
+  display: table;
+}
+
+.status {
+  margin - bottom: 10px;
+}
+
+.square {
+  background: #fff;
+  border: 1px solid #999;
+  float: left;
+  font - size: 24px;
+  font - weight: bold;
+  line - height: 34px;
+  height: 34px;
+  margin - right: -1px;
+  margin - top: -1px;
+  padding: 0;
+  text - align: center;
+  width: 34px;
+}
+
+.square: focus {
+  outline: none;
+}
+
+.kbd - navigation.square: focus {
+  background: #ddd;
+}
+
+.game {
+  display: flex;
+  flex - direction: row;
+}
+
+.game - info {
+  margin - left: 20px;
+}
