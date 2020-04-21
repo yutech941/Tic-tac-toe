7b109e8af845bfee55cbc79c629061c5b3b7112a
小林優一 <y.k94cofitec179@gmail.com>
2020-04-21 14:50:25 +0900

    コンストラクタ作成

--- src/index.js
+++ src/index.js
@@ -2,19 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
 class Square extends React.Component {
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square"
+        onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
@@ -41,7 +56,8 @@ class Board extends React.Component {
       </div>
     );
   }
-}
+};
+
 
 class Game extends React.Component {
   render() {
